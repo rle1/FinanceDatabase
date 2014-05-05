732a89443c5bc94b63f05e344199de14b6e1e25e
Anh Tran <anhxuantran09@gmail.com>
2014-05-04 20:53:16 -0400

    Revert "remove date checking"
    This reverts commit 28548d8a88dda1ab33da798c6f0127cded98c22d.

--- csvfilereader.php
+++ csvfilereader.php
@@ -110,6 +110,9 @@
 								$currentQuote = mysqli_query($con, "SELECT Quote From quotes WHERE Date='$newDate' AND Stock_name='$stock'");
 								$row = $currentQuote->fetch_assoc();
 								$sellQuote = $row['Quote'];
+									/************************************************************************************************
+						not sure if we should check if buyQuote or sellQuote is zero
+					**************************************************************************************************/
 								if(empty($row) || $sellQuote <=0){
 									continue;
 								}
@@ -118,9 +121,27 @@
 								$dateRow = $buyDateQuery->fetch_assoc();
 								$buyDate = $dateRow['Date'];
 
+								if(empty($dateRow)){
+									continue;
+								}
+
 								$prevQuote = mysqli_query($con, "SELECT Quote FROM quotes WHERE Date = '$buyDate' AND Stock_name = '$stock'");
 								$row = $prevQuote->fetch_assoc();
 								$buyQuote = $row['Quote'];
+									/************************************************************************************************
+						not sure if we should check if buyQuote or sellQuote is zero
+					**************************************************************************************************/
+								if(empty($row) || $buyQuote <= 0){
+									continue;
+								}
+
+								$appFactor = $sellQuote / $buyQuote;
+
+								$totalPortInvestment += $percentage;
+
+								$moneyInvested = $totalCash * $percentage;
+
+								$totalFundValue = $totalFundValue + ($moneyInvested * $appFactor);
 							}
 
 							$totalFundValue = $totalFundValue + ($totalCash * (1.0 - $totalPortInvestment));
@@ -177,6 +198,10 @@
 					$buyDateQuery = mysqli_query($con, "SELECT Date FROM portfolio_act_stocks WHERE Portfolio_ID = '$portID' AND Stock_name='$gettingSold' AND Buy_or_sell='B'");
 					$dateRow = $buyDateQuery->fetch_assoc();
 					$buyDate = $dateRow['Date'];
+
+					if(empty($dateRow)){
+						continue;
+					}
 
 					//find quote on date portfolio bought stock
 					$prevQuote = mysqli_query($con, "SELECT Quote FROM quotes WHERE Date = '$buyDate' AND Stock_name = '$gettingSold'");
@@ -345,7 +370,7 @@
 						$stockNumRow = $stockNumQuery->fetch_assoc();
 						$numStocks = $stockNumRow['Num_stocks'];
 
-						if($numStocks <= 0 || empty($stockNumRow)){
+						if($numStocks <= 0 || empty($stockNumRow){
 							continue;
 						}
 						
@@ -441,6 +466,10 @@
 								$buyDateQuery = mysqli_query($con, "SELECT Date FROM portfolio_act_stocks WHERE Portfolio_ID = '$portID' AND Stock_name='$stock' AND Buy_or_sell='B'");
 								$dateRow = $buyDateQuery->fetch_assoc();
 								$buyDate = $dateRow['Date'];
+
+								if(empty($dateRow)){
+									continue;
+								}
 
 								$prevQuote = mysqli_query($con, "SELECT Quote FROM quotes WHERE Date = '$buyDate' AND Stock_name = '$stock'");
 								$row = $prevQuote->fetch_assoc();
@@ -542,6 +571,10 @@
 					$dateRow = $buyDateQuery->fetch_assoc();
 					$buyDate = $dateRow['Date'];
 
+					if(empty($dateRow)){
+						continue;
+					}
+					
 					//find quote on date portfolio bought stock
 					$prevQuote = mysqli_query($con, "SELECT Quote FROM quotes WHERE Date = '$buyDate' AND Stock_name = '$selling'");
 					$row = $prevQuote->fetch_assoc();
